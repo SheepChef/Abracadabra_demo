5238d3826d096b324e7d114dc7a1ea1086d40fe2
SheepChef <wuyuyang2007@gmail.com>
2025-04-11 19:43:41 +0800

    V3.1.0

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,34 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
-import Userscript from 'vite-userscript-plugin';
+import Userscript from "vite-userscript-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // 配置选项
+  // 配置选项
   plugins: [
     Userscript({
-      entry: 'src/index.js',
+      entry: "src/index.js",
       header: {
-        name:"Abracadabra 魔曰",
-        description:"文本防和谐工具组件，由 Abracadabra 项目支持。",
-        version:"v3.0.11",
-        "run-at":"document-idle",
-        supportURL:"https://github.com/SheepChef/Abracadabra",
-        author:"SheepChef",
-        icon64:"https://abracadabra-demo.pages.dev/assets/favicon.png",
-        namespace:"shef.cc",
-        license:"GPLv3",
+        name: "Abracadabra 魔曰",
+        description: "文本防和谐工具组件，由 Abracadabra 项目支持。",
+        version: "v3.1.0",
+        "run-at": "document-idle",
+        supportURL: "https://github.com/SheepChef/Abracadabra",
+        author: "SheepChef",
+        icon64: "https://abracadabra-demo.pages.dev/assets/favicon.png",
+        namespace: "shef.cc",
+        license: "GPLv3",
         match: [
-          'https://tieba.baidu.com/*',
-          'https://www.bilibili.com/video/*'
-        ]
+          "https://tieba.baidu.com/*",
+          "https://www.bilibili.com/video/*",
+        ],
       },
       server: {
-        port: 3000
+        port: 3000,
       },
-      esbuildTransformOptions:{
-        minify:false
-      }
-    })
-  ]
+      esbuildTransformOptions: {
+        minify: false,
+      },
+    }),
+  ],
 });
