d8b82fcb33035a16930f0cd44222880916baa02d
SheepChef <wuyuyang2007@gmail.com>
2025-05-03 21:09:31 +0800

    Update V3.1.2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -577,7 +577,7 @@ onBeforeUnmount(() => {});
             margin: 0px;
           "
         >
-          Abracadabra V3.1.1<br /><a href="https://github.com/SheepChef/Abracadabra">Github Repo</a>
+          Abracadabra V3.1.2<br /><a href="https://github.com/SheepChef/Abracadabra">Github Repo</a>
         </p>
         <p
           style="
