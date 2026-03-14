569eb756b81839a51ac81ac755beaff88fed7be7
SheepChef <wuyuyang2007@gmail.com>
2026-03-15 01:34:20 +0800

    V3.3.3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -586,10 +586,26 @@ async function ProcessDecNext() {
   ExecuteCounter.value++;
   let Abra = new Abracadabra(InputMode.value, OutputMode.value);
   let key;
+
   try {
     if (InputMode.value == "TEXT") {
       if (document.getElementById("InputCard").value == "") {
         return;
+      }
+      if (document.getElementById("InputCard").value[0] == "呋") {
+        try {
+          Abra.BearDecode(document.getElementById("InputCard").value);
+          document.getElementById("OutputText").value = Abra.Output();
+
+          snackbar({
+            message: "识别到熊曰密文，已成功解密。",
+            autoCloseDelay: 1500
+          });
+
+          return;
+        } catch (err) {
+          //Do Nothing
+        }
       }
       if (document.getElementById("KeyCard").value == "") {
         key = "ABRACADABRA";
@@ -976,7 +992,7 @@ onBeforeUnmount(() => {});
             margin: 0px;
           "
         >
-          Abracadabra V3.3.1<br /><a style="color: #637eff">Offline Build</a>
+          Abracadabra V3.3.3<br /><a style="color: #637eff">Offline Build</a>
         </p>
         <p
           id="CopyRightSheepChef"
